43d86b15bd8db2dff7605e152f2a8ad6fb594587
swardbladpaulo <paulo@swardblads.se>
2020-11-04 17:48:11 +0100

    modified Footer.jsx, Hello.jsx, index.css for semantic ui, row 2 in Footer.jsx import Container is not used, run cy;open, all good

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 
 const Hello = () => {
   return (
-    <div>
+    <Container >
       <h1 id="hello">Hello World</h1>
-    </div>
+    </Container>
   );
 };
 
