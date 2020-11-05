73f6e42b657830df9a29c78a94bcfb4de7c8eaf6
swardbladpaulo <paulo@swardblads.se>
2020-11-05 21:34:12 +0100

    modified projects.json, mkdir data/images, modified project.jsx, add file projectcards.jsx and modified, run cypress, all good.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Grid } from 'semantic-ui-react';
+import ProjectCard from './ProjectCard';
 import axios from 'axios';
 
 class Projects extends Component {
@@ -19,7 +20,7 @@ class Projects extends Component {
     let projectsList = projects.map((project) => {
       return (
         <div id={`project-${project.id}`} key={project.id}>
-          <h3>{project.name}</h3>
+          <ProjectCard project={project}/>
         </div>
       );
     });
@@ -27,7 +28,7 @@ class Projects extends Component {
     return (
       <Container>
         <h1 id="projects-header">My Projects</h1>
-        {projectsList}
+        <Grid>{projectsList}</Grid>
       </Container>
     );
   };
