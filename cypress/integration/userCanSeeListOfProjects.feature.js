e4eaea81e1bf8ee3aa679825a9e93c66e42de882
swardbladpaulo <paulo@swardblads.se>
2020-11-05 20:54:08 +0100

    Write test for ‘displays the first project’ modified the project.jsx with adding class, state, new variable projectList, iterate using .map, add id key added axios, mkdir public and projects.json file, modified projects.json and projects.jsx, run cypress, fail displays first project

---
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -0,0 +1,16 @@
+describe('User can see list of project', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get('#projects-tab').click();
+  });
+
+  it('displays the first project', () => {
+    cy.get('#project-1').within(() => {
+      cy.get('.image').should("exist");
+      cy.get('.header').should("contain", "My First Website");
+      cy.get('.description').should("contain", "This was my first project");
+    });
+  });
+  
+  
+});
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
+import axios from 'axios';
 
-const Projects = () => {
+class Projects extends Component {
+  state = {
+    projects: [],
+  };
+
+  componentDidMount() {
+    axios.get("./data/projects.json").then((response) => {
+      this.setState({ projects: response.data })
+    });
+  }
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  };
+};
+
+export default Projects;
+
+/*const Projects = () => {
   return (
     <Container>
       <h1 id="projects-header">My Projects</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, doloremque!</p>
     </Container>
   );
-};
-
-export default Projects;
+};*/
