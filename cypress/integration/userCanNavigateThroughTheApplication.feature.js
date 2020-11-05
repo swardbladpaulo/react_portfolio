5d944211e346c9c14a73942bff9c4c99df7f2ab0
swardbladpaulo <paulo@swardblads.se>
2020-11-05 10:12:25 +0100

    completed and modified the cypress test for userCanNavigateThroughTheApplication.feature, run test, failed

--- cypress/integration/userCanNavigateThroughTheApplication.feature.js
+++ cypress/integration/userCanNavigateThroughTheApplication.feature.js
@@ -3,23 +3,73 @@ describe('User can navigate the app', () => {
     cy.visit("http://localhost:3000");
   });
 
-  it('if select About Me on the top menu in the header.jsx and no longer see the hello.jsx', () => {
-    /*cy.get('#about-tab').click();*/
+  describe('to About tab and it', () => {
+    beforeEach(() => {
+      cy.get('#about-tab').click();
+    });
+    
+    it('displays About Me header', () => {
+      cy.get('#about-header').should("contain", "About Me");
+    });
 
-    cy.get('#about-header').should("contain", "About Me");
+    it('displays component name in url', () => {
+      cy.url().should("contain", "about");
+    });
 
-    /*cy.get('#projects-header').should("not.exist");*/
-    /*cy.get('#hello').should("not.exist");*/
+    it('does not display My Projects header', () => {
+      cy.get('#project-header').should("not.exist");
+    });
+
+    it('does not display Hello World', () => {
+      cy.get('#hello').should("not.exist");
+    });
   });
 
-  it('if select My Projects on the top menu in the header.jsx', () => {
-    /*cy.get('#projects-tab').click();*/
+  describe('to My Project tab and it', () => {
+    beforeEach(() => {
+      cy.get("#project-tab").click();
+    });
 
-    cy.get('#project-header').should("contain", "My Projects");
+    it('displays My Project header', () => {
+      cy.get('#project-header').should("contain", "My Projects");
+    });
 
-    /*cy.get('#about-header').should("not.exist");*/
-    /*cy.get('#hello').should("not.exist");*/
+    it('displays component name in url', () => {
+      cy.url().should("contain", "projects");
+    });
+
+    it('does not displays About Me header', () => {
+      cy.get('#about-header').should("not.exist");
+    });
+
+    it('does not displays Hello World', () => {
+      cy.get('#hello').should("not.exist");
+    });
   });
 
-})
+  describe('back to My Portfolio/Hello tab and it', () => {
+    beforeEach(() => {
+      cy.get("#about-tab").click();
+      cy.get("#header").click();
+    });
+
+    it('displays Hello World', () => {
+      cy.get('#hello').should("contain", "Hello");
+    });
+
+    it('displays correct url', () => {
+      cy.url()
+        .should("not.contain", "projects")
+        .and("not.contain", "about");
+    });
+
+    it('does not displays About Me header', () => {
+      cy.get('#about-header').should("not.exist");
+    });
+
+    it('does not display My Projects header', () => {
+      cy.get('#project-header').should("not.exist");
+    });
+  });
+});
 
