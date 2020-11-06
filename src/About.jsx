3a5f22eebb5eac4d672fb9addd1d70f5f0f745c3
swardbladpaulo <paulo@swardblads.se>
2020-11-06 17:56:32 +0100

    Modified about.jsx with styling, added semantic ui grids

--- src/About.jsx
+++ src/About.jsx
@@ -7,12 +7,33 @@ const About = () => {
       <h1 id="about-header">About Me</h1>
       
       <h3 className="ui dividing header">General</h3>
-  
-    
-    
-    
-    
-      <img class="ui fluid image" src="./data/images/csslogo.png"></img>
+      <div className="ui stackable four column grid">
+        <div className="column">Column 1</div>
+        <div className="column">Column 2</div>
+        <div className="column">Column 3</div>
+        <div className="column">Column 4</div>
+      </div>
+
+      <h3 className="ui dividing header">B</h3>
+      <div className="ui internally celled grid">
+        <div className="row">
+
+          <div className="three wide column">
+            <img src="./data/images/bmicalc.png"></img>
+          </div>
+            
+          <div className="ten wide column">
+            <p>Add text 1</p>
+            </div>
+            
+          <div className="three wide column">
+            <img src="./data/images/fizzbuzz.png"></img>
+          </div>
+          
+          <h3 className="ui dividing header">C</h3>
+            
+        </div>
+      </div>
     </Container>
   );
 };
